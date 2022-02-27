c8e4e9c59d467c3962410b5bc7a0f49d91f81405
Afnan <afnantkib@gmail.com>
2022-02-27 23:03:41 +0530

    Added increment and decrement functions in cart items

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,54 @@ import React from "react";
 
 
 class CartItem extends React.Component {
+    constructor (){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+
+
+
+    }
+
+    increaseQty =()=>{
+     this.setState((prevState)=>{
+        return { qty:prevState.qty+1}
+     })
+    }
+    decreaseQty =()=>{
+     this.setState((prevState)=>{
+        return { qty:prevState.qty-1}
+     })
+    }
+    
+
+
+
+
     render() {
+        const {price,title,qty}=this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:"grey"}}>Rs 999</div>
-                    <div style={{color:"grey"}}>Qty: 1 </div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:"grey"}}>Rs {price}</div>
+                    <div style={{color:"grey"}}>{qty}</div>
                     <div className="cart-item-actions">
-                        {/* Buttons*/}
+                    <img className="action-icons"
+                    onClick={this.increaseQty}
+                     src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1645978476~hmac=ca2da12ec1f1a75459f7cc5523eae61a"/>
+                    <img className="action-icons" 
+                    onClick={this.decreaseQty}
+                    src="https://cdn-icons-png.flaticon.com/512/992/992683.png "/>
+                    <img className="action-icons" 
+                    src="https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1645978566~hmac=df7a6a4a064aece6b8dda51a6d6b97dd"/>
 
                     </div>
 
